c55034a768c474d326ebe3e90bffc9220a3d18dc
wonderfulroller7 <sb6856@nyu.edu>
2019-10-28 18:34:39 -0400

    Initialising the database

--- database.py
+++ database.py
@@ -8,12 +8,16 @@ DATABASE = ''
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config('DATABASE')
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
         )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
 
 def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    
+    db = get_db()
+    with current_app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
