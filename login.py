b30f3ca6dc978fca1301e0e2922140f0c2da1ecb
wonderfulroller7 <sb6856@nyu.edu>
2019-10-28 20:34:03 -0400

    Registraion logic added

--- login.py
+++ login.py
@@ -1,4 +1,8 @@
+# Library imports
 from flask import Blueprint, render_template, redirect, g, url_for, request
+from werkzeug import generate_password_hash
+
+# Local file imports
 from database import get_db
 
 root_view = Blueprint('login', __name__, url_prefix='')
@@ -9,9 +13,31 @@ def basepath():
 
 @root_view.route('/register', methods=['GET', 'POST'])
 def register():
-
+    print(request.method)
     if request.method == 'POST':
-        return render_template('/registrationFailure.html')
+        select_query = 'SELECT id FROM user WHERE name = ?'
+        insert_query = 'INSERT INTO user(user, password, phone) VALUES (?, ?, ?)'
+        user = request.form['uid']
+        password = request.form['password']
+        phone = request.form['doublefactor']
+        db_connection = get_db()
+        print(user, password, phone)
+        error = None
+        if not user:
+            error = 1
+        elif not password:
+            error = 1
+        elif db_connection.execute(select_query, (user,)).fetchone() is not None:
+            error = 'User {} is already registered.'.format(user)
+        print(error)
+        if error is not None:
+            db_connection.execute(insert_query,(user, generate_password_hash(password), generate_password_hash(phone)))
+            db_connection.commit()
+            print(user, generate_password_hash(password), generate_password_hash(phone))
+            return render_template('/success.html')
+        else:
+            return render_template('/failure.html')
+
     return render_template('/register.html')
 
 @root_view.route('/login', methods=['GET', 'POST'])
