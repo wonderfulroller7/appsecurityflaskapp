5c87e3956ff9dac7f53df97756cafcf63c9b1288
wonderfulroller7 <sb6856@nyu.edu>
2019-11-13 18:37:13 -0500

    All tests updated

--- test_app.py
+++ test_app.py
@@ -16,8 +16,16 @@ def test_login_get(client, app):
     assert client.get('/login').status_code == 200
 
 def test_login_post(client, app):
+    client.post('/register', data = {'uname': 'sourbose', 'pword': 'test123', '2fa': '9292197847'})
     client.post('/login', data = {'uname': 'sourbose', 'pword': 'test123', '2fa': '9292197847'})
     with client:
-        assert client.get('/spell_check').status_code == 302
+        assert client.get('/login').status_code == 302
+        assert client.get('/spell_check').status_code == 200
         assert g.user['uname'] == 'sourbose'
-
+        
+def test_csrf(client, app):
+    client.post('/register', data = {'uname': 'sourbose', 'pword': 'test123', '2fa': '9292197847'})
+    client.post('/login', data = {'uname': 'sourbose', 'pword': 'test123', '2fa': '9292197847'})
+    with client:
+        response = client.get('/spell_check')
+        print(response.data)
